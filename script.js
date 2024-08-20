84000adb9f9a5b086e6ac940ba053f549e0fbda8
Honda Coding <pedrohonda03@gmail.com>
2024-08-20 17:08:52 -0300

    Adicionado Modal 'Trajetória da DD'(sem responsividade)

--- script.js
+++ script.js
@@ -11,3 +11,34 @@ function scrollToSection(sectionId) {
         section.scrollIntoView({ behavior: 'smooth' }); // Rolagem suave para a seção
     }
 }
+
+// ----------------- MODAL --------------------
+document.addEventListener('DOMContentLoaded', function() {
+    const modal = document.getElementById('myModal');
+    const closeBtn = document.querySelector('.close-btn');
+    const openBtns = document.querySelectorAll('.open-modal-btn');
+    const body = document.body;
+
+    // Abre o modal quando qualquer botão é clicado
+    openBtns.forEach(button => {
+        button.addEventListener('click', function() {
+            modal.style.display = 'block';
+            body.classList.add('no-scroll'); // Adiciona a classe para desativar a rolagem
+        });
+    });
+
+    // Fecha a modal quando o botão "X" é clicado
+    closeBtn.addEventListener('click', function() {
+        modal.style.display = 'none';
+        body.classList.remove('no-scroll');
+    });
+
+    // Fecha a modal quando se clica fora da área de conteúdo da modal
+    window.addEventListener('click', function(event) {
+        if (event.target === modal) {
+            modal.style.display = 'none';
+            body.classList.remove('no-scroll');
+        }
+    });
+});
+

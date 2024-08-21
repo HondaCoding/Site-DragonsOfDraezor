35050e0a772b130127627e00c015b57a85a9b3af
Honda Coding <pedrohonda03@gmail.com>
2024-08-21 12:17:34 -0300

    Site finalizado (sem responsividade)

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ function scrollToSection(sectionId) {
     }
 }
 
+
 // ----------------- MODAL --------------------
 document.addEventListener('DOMContentLoaded', function() {
     const modal = document.getElementById('myModal');
@@ -19,21 +20,21 @@ document.addEventListener('DOMContentLoaded', function() {
     const openBtns = document.querySelectorAll('.open-modal-btn');
     const body = document.body;
 
-    // Abre o modal quando qualquer botão é clicado
+    // Abre o modal
     openBtns.forEach(button => {
         button.addEventListener('click', function() {
             modal.style.display = 'block';
-            body.classList.add('no-scroll'); // Adiciona a classe para desativar a rolagem
+            body.classList.add('no-scroll');
         });
     });
 
-    // Fecha a modal quando o botão "X" é clicado
+    // Fecha a modal
     closeBtn.addEventListener('click', function() {
         modal.style.display = 'none';
         body.classList.remove('no-scroll');
     });
 
-    // Fecha a modal quando se clica fora da área de conteúdo da modal
+    // Fecha a modal quando clica fora
     window.addEventListener('click', function(event) {
         if (event.target === modal) {
             modal.style.display = 'none';
@@ -42,3 +43,27 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
+
+// -------------------- ICON BACK TOPO --------------------
+document.addEventListener('DOMContentLoaded', function() {
+    const backToTopButton = document.getElementById('back-to-top');
+
+    function toggleButtonVisibility() {
+        if (window.scrollY > 600) {
+            backToTopButton.style.display = 'block';
+        } else {
+            backToTopButton.style.display = 'none';
+        }
+    }
+
+    // Evento de rolagem para mostrar ou esconder
+    window.addEventListener('scroll', toggleButtonVisibility);
+
+    // Volta para o topo de forma suave
+    backToTopButton.addEventListener('click', function() {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    });
+});

34c225ed81554805b93c42ee5c33783f6bc7afe5
Honda Coding <pedrohonda03@gmail.com>
2024-08-26 15:43:07 -0300

    Adicionado responsividade e menu hamburguer

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ function redirectToURL(url) {
     window.open(url='https://discord.gg/mUxBxHUzyq', '_blank');
 }
 
-
 // ------------- BOTOES DO NAV ---------------
 function scrollToSection(sectionId) {
     const section = document.getElementById(sectionId);
@@ -11,7 +10,6 @@ function scrollToSection(sectionId) {
         section.scrollIntoView({ behavior: 'smooth' }); // Rolagem suave para a seção
     }
 }
-
 
 // ----------------- MODAL --------------------
 document.addEventListener('DOMContentLoaded', function() {
@@ -43,13 +41,12 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
-
 // -------------------- ICON BACK TOPO --------------------
 document.addEventListener('DOMContentLoaded', function() {
     const backToTopButton = document.getElementById('back-to-top');
 
     function toggleButtonVisibility() {
-        if (window.scrollY > 600) {
+        if (window.scrollY > 450) {
             backToTopButton.style.display = 'block';
         } else {
             backToTopButton.style.display = 'none';
@@ -65,5 +62,34 @@ document.addEventListener('DOMContentLoaded', function() {
             top: 0,
             behavior: 'smooth'
         });
+    });
+});
+
+// -------------------- MENU MOBILE ------------------
+document.addEventListener('DOMContentLoaded', function() {
+    const menuToggle = document.getElementById('menuToggle');
+    const navMenu = document.getElementById('navMenu');
+    const body = document.getElementById('body');
+    
+    
+    // Função para alternar a visibilidade do menu
+    function toggleMenu() {
+        if (navMenu.style.display === 'block') {
+            closeMenu();
+        } else {
+            navMenu.style.display = 'block';
+        }
+        menuToggle.style.zIndex = 999;
+    }
+    
+    // Função para fechar o menu
+    function closeMenu() {
+        navMenu.style.display = 'none';
+    }
+
+    // Adiciona o evento de clique ao botão de menu
+    menuToggle.addEventListener('click', function(event) {
+        event.stopPropagation();
+        toggleMenu();
     });
 });
